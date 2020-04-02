0ee5d95b3ca4981b18bfa0f36f06825c5126ecac
hongngat <48482193+hongngat@users.noreply.github.com>
2020-04-02 19:08:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,3 +88,16 @@ $(document).ready(function(e){
   $('.input-group #search_param').val(param);
 });
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 99) {
+      $('#nav').addClass('navbar-fixed-top');
+  }
+
+
+  if ($(window).scrollTop() >= 100) {
+      $('#nav').addClass('show');
+  } else {
+      $('#nav').removeClass('show navbar-fixed-top');
+  }
+});
